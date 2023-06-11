100611335523c628b0d5bc52e7d94c38bd2c5b76
Rebecca Chan <125217883+rebeccachan13@users.noreply.github.com>
2023-06-10 21:04:34 -0600

    Initial commit

---
+++ Number Guessing Game/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int winNum = r.Next(0,100);
+
+            bool win = false;
+
+            do
+            {
+                Console.Write("Guess a number integer between 0 and 100: ");
+                string s = Console.ReadLine();
+
+                int i = int.Parse(s);
+
+                if (i > winNum)
+                {
+                    Console.WriteLine("Too high! Guess lower....");
+                }
+                else if (i < winNum)
+                {
+                    Console.WriteLine("Too low! Guess higher....");
+                }
+                else
+                {
+                    Console.WriteLine("Congratulations! You Win!");
+                    win = true;
+                }
+                Console.WriteLine();
+            } while (win == false);
+
+            Console.WriteLine("Thank you for playing the game!");
+            Console.Write("Press any key to finish....");
+            Console.ReadKey();
+        }
+        
+    }
+}
